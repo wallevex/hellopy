88318334a65d570ee2f8ab540572b5013c584171
huhaolong <wallevex@gmail.com>
2025-05-26 16:01:18 +0800

    learn FunASR

---
+++ cosyvoicev2.py
@@ -0,0 +1,3 @@
+# SDK模型下载
+from modelscope import snapshot_download
+snapshot_download('iic/CosyVoice2-0.5B', local_dir='iic/CosyVoice2-0.5B')
--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("hello world")
+import torch
+
+print("CUDA 可用：", torch.cuda.is_available())
+print("CUDA 版本：", torch.version.cuda)
+print(torch.backends.cudnn.enabled)
+print(torch.backends.cudnn.version())
+
+# # 变量定义
+# x = 10          # 整数
+# y = 3.14         # 浮点数
+# name = "Alice"   # 字符串
+# is_active = True # 布尔值
+#
+# # 多变量赋值
+# a, b, c = 1, 2, "three"
+#
+# # 查看数据类型
+# print(type(x))        # <class 'int'>
+# print(type(y))        # <class 'float'>
+# print(type(name))     # <class 'str'>
+# print(type(is_active)) # <class 'bool'>
+# list = ['a', 'b', 'c']
+# print(list * 2)
